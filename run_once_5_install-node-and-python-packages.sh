319f77dc7fdde82e2c5920d6f3f6c318c5fcfc5b
Rai Butera <rai@rbutera.com>
2022-07-09 16:44:03 +0100

    add node and python packages

---
+++ run_once_5_install-node-and-python-packages.sh
@@ -0,0 +1,10 @@
+# install global node packages
+nodenv install 16.13.1
+nodenv global 16.13.1
+npm install -g @nestjs/cli prettier eslint typescript xo markdownlint remark hicat ts-node
+
+# install global python packages
+pyenv install 3.10.4
+pyenv global 3.10.4
+pip install autopep8 flake8 black click
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
