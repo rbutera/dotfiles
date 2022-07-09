275f54e856865a29e78c5d1d4345317a16413238
Rai Butera <rai@rbutera.com>
2022-07-09 16:53:29 +0100

    fix node and python again

--- run_once_5_install-node-and-python-packages.sh
+++ run_once_5_install-node-and-python-packages.sh
@@ -5,6 +5,15 @@ nodenv global 16.13.1
 npm install -g @nestjs/cli prettier eslint typescript xo markdownlint remark hicat ts-node
 
 # install global python packages
+{{ if eq .chezmoi.os "linux"}}
+  {{ if eq .chezmoi.osRelease "ubuntu" }}
+sudo apt-get update; sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+  {{ else }}
+sudo pacman -S --needed base-devel openssl zlib xz tk
+  {{end}}
+{{end}}
 pyenv install 3.10.4
 pyenv global 3.10.4
 pip install autopep8 flake8 black click
